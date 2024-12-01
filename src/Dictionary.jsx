a40f29bf310565ccb6927d0aa4530f9a40e2b487
anniesalis <anniesalis02@gmail.com>
2024-12-01 17:34:53 +0000

    Added API integration

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,18 +1,32 @@
 import React, {useState} from "react"
+import axios from "axios";
+
 
 export default function Dictionary() {
 
 const [word, setWord]=useState("")
 
+function displayResponse(response) {
+
+
+}
+
+
 function search(event) {
 event.preventDefault();
-alert(`searching for the meanng of "${word}"`);
+
+
+let apiKey = "2ob113a879d9f74f53b31fb0t04ab5cb"
+let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}` 
+console.log(apiUrl);
+axios.get(apiUrl).then(displayResponse);
+console.log(displayResponse.data)
 }
 
 function handleChange(event) {
     
 setWord(event.target.value);
-console.log(event.target.value);
+
 }
 
 
