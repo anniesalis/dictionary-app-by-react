5511a5043653c623a2ae119327b36befacf247d5
anniesalis <anniesalis02@gmail.com>
2024-12-07 15:58:44 +0000

    .

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,13 +1,14 @@
 import React, {useState} from "react"
 import axios from "axios";
-
+import "./Dictionary.css"
 
 export default function Dictionary() {
 
-const [word, setWord]=useState("")
+const [word, setWord]=useState("");
+
 
 function displayResponse(response) {
-
+console.log(response.data);
 
 }
 
@@ -15,14 +16,17 @@ function displayResponse(response) {
 function search(event) {
 event.preventDefault();
 
+if (word) {
+    alert("Please enter a word to search");
+return;
+}
+
 
 let apiKey = "2ob113a879d9f74f53b31fb0t04ab5cb"
 let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}` 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayResponse);
-console.log(displayResponse.data)
 }
-
 function handleChange(event) {
     
 setWord(event.target.value);
@@ -34,7 +38,13 @@ return (
     
    <div className="dictionary"> 
     <form onSubmit={search}>
-         <input type="search" onChange={handleChange} />
+         <input type="search" 
+         onChange={handleChange} 
+         placeholder="Enter a Word" 
+         required />
+    
+     <input type="submit"
+      value="search" />
      </form>
      </div>
  
